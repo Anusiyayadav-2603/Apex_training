d7faefab0819f9e7327c5b7c3abb98bf8422d9c4
Anusiyayadav-2603 <anu147750@gmail.com>
2026-09-25 13:37:04 +0530

    DAY-03-MARKS

---
+++ DAY-03/DAY-3-MARKS.py
@@ -0,0 +1,8 @@
+Java=float(input("Enter the value:"))
+Python=float(input("Enter the value:"))
+MySQL=float(input("Enter the value:"))
+print("Total:",Java+Python+MySQL)
+print("Average:",Java+Python+MySQL/3)
+print("Percentage:",Java+Python+MySQL/300*100)
+print("Remaining marks:",300-Java+Python+MySQL)
+ 
